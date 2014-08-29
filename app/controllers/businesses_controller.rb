81a70d6539126586ae4c83e7adda1010022b5378
ragarwal91 <ranjan@ranjandesign.com>
2014-08-28 23:04:57 -0400

    new business and sales forms

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -4,4 +4,23 @@ class BusinessesController < ApplicationController
     @business = Business.find(params[:id])
   end  
 
+  def new
+    @business = Business.new
+  end
+
+  def create
+    @business = Business.new(business_params)
+    if @business.save
+      session[:current_user] = @business.id
+      redirect_to(root_path)
+    else
+      render(:new)
+    end
+  end
+
+  private
+  def business_params
+    params.require(:business).permit(:name, :address, :city, :zip_code, :logo)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get "/"                  => 'welcome#index', as: 'root'
   resources :users, except: [:index]
-  resources :businesses, except: [:index]
-  resources :sales, except: [:index]
+  resources :businesses, except: [:index] do
+    resources :sales, except: [:index]
+  end
   # /my_sales
 end
